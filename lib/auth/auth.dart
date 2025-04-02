68054788158488c15c56d47972a0af5cfd86d327
AustinB12 <austinzbaird@gmail.com>
2025-04-01 22:41:09 -0400

    Update auth.dart

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -11,9 +11,13 @@ class AuthService {
         .signInWithPassword(email: email, password: password);
   }
 
-  Future<AuthResponse> signUpWithEmailPassword(
-      String email, String password) async {
-    return await _client.auth.signUp(email: email, password: password);
+  Future<String?> signUpWithEmailPassword(String email, String password) async {
+    AuthResponse response =
+        await _client.auth.signUp(email: email, password: password);
+    if (response.user != null) {
+      return response.user?.id;
+    }
+    return null;
   }
 
   Future<void> signOut() async {
@@ -30,6 +34,16 @@ class AuthService {
     final session = _client.auth.currentSession;
     final user = session?.user;
     return user?.id;
+  }
+
+  void createNewUserProfile(String id) async {
+    print('here\n\n');
+    await _client.from('User_Profiles').insert({
+      'first_name': null,
+      'last_name': null,
+      'profile_picture': null,
+      'auth_id': id
+    });
   }
 
 //! ============= Data =============
