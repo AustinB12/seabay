9d7b43b786b414a5dc2a8ea9cbfa6fe7bb49ac2c
AustinB12 <austinzbaird@gmail.com>
2025-05-01 16:33:45 -0400

    renaming

---
+++ test/wishlist_test.dart
@@ -0,0 +1,21 @@
+// import 'package:seabay_app/api/posts.dart';
+// import 'package:test/test.dart';
+
+// final testValue = "im a different";
+
+// void main() {
+//   test('Should be able to update Post description', () {
+//     final post = Post(title: 'test', isActive: true, userId: '123');
+
+//     post.description = testValue;
+
+//     expect(post.description, testValue);
+//   });
+//   test('Should be able to update Post title', () {
+//     final post = Post(title: 'test', isActive: true, userId: '123');
+
+//     post.title = testValue;
+
+//     expect(post.title, testValue);
+//   });
+// }
