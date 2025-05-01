acc2cdefe344bf3efe084b528da47ca61cd86c3b
AustinB12 <austinzbaird@gmail.com>
2025-05-01 16:39:51 -0400

    Update wishlist_test.dart

--- test/wishlist_test.dart
+++ test/wishlist_test.dart
@@ -1,21 +1,21 @@
-// import 'package:seabay_app/api/posts.dart';
-// import 'package:test/test.dart';
+import 'package:seabay_app/api/wishlists.dart';
+import 'package:test/test.dart';
 
-// final testValue = "im a different";
+final testValue = "im a different";
 
-// void main() {
-//   test('Should be able to update Post description', () {
-//     final post = Post(title: 'test', isActive: true, userId: '123');
+void main() {
+  test('Should be able to update Post description', () {
+    final wl = WishList(id: 1, name: 'yo', userId: '123', description: 'desc');
 
-//     post.description = testValue;
+    wl.description = testValue;
 
-//     expect(post.description, testValue);
-//   });
-//   test('Should be able to update Post title', () {
-//     final post = Post(title: 'test', isActive: true, userId: '123');
+    expect(wl.description, testValue);
+  });
+  test('Should be able to update Post title', () {
+    final wl = WishList(id: 1, name: 'yo', userId: '123', description: 'desc');
 
-//     post.title = testValue;
+    wl.name = testValue;
 
-//     expect(post.title, testValue);
-//   });
-// }
+    expect(wl.name, testValue);
+  });
+}
